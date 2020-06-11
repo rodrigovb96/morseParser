42707727ff7ad77633bc7204a94c33c2d8d8912c
joao Castro <joao_lukas97@hotmail.com>
2020-06-11 14:29:35 -0300

    pasta publica

---
+++ files/templates/js/index.js
@@ -0,0 +1,42 @@
+(function () {
+    // your page initialization code here
+    // the DOM will be available here
+    console.log('loaded page');
+})();
+
+function fallbackCopyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(element) {
+    var text = element.textContent;
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    }
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Async: Copying to clipboard was successful!');
+    }, function (err) {
+        console.error('Async: Could not copy text: ', err);
+    });
+}
--- webserver/webserver.go
+++ webserver/webserver.go
@@ -94,6 +94,7 @@ func determineListenAddress() (string,error) {
 func InitServer() {
 	http.HandleFunc("/",indexPage)
 	http.HandleFunc("/parse/",parsePage)
+	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("files"))))
 
 	port, err := determineListenAddress()
 
